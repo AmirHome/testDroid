fe0a49ce6e773c28037966f4eab99a25c208b0fc
Amir Hosseinzadeh <amir.email@yahoo.com>
2016-08-15 11:53:39 +0300

    scroll view

--- app/src/main/java/com/amirhome/myapplication/MainActivity.java
+++ app/src/main/java/com/amirhome/myapplication/MainActivity.java
@@ -3,11 +3,13 @@ package com.amirhome.myapplication;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView tv = (TextView) findViewById(R.id.longText);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(getString(R.string.lorem_ipsum));
+        sb.append(getString(R.string.lorem_ipsum));
+        sb.append(getString(R.string.lorem_ipsum));
+
+        tv.setText(sb.toString());
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
